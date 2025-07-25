cab737a9211a01b86c77c5c35003fe76ba629ac2
Eduardo Marquez <oeo28@txstate.edu>
2025-07-24 21:00:26 -0500

    Initial commit

---
+++ src/main/java/com/gridapp/territory_mapper/TerritoryMapperApplication.java
@@ -0,0 +1,13 @@
+package com.gridapp.territory_mapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerritoryMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerritoryMapperApplication.class, args);
+	}
+
+}
