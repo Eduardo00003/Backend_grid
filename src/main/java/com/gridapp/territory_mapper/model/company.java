b4673be450fce7df96cfdc41b4177bd4e77b02c4
Eduardo Marquez <oeo28@txstate.edu>
2025-07-30 22:04:53 -0500

    Finsih it

--- src/main/java/com/gridapp/territory_mapper/model/company.java
+++ src/main/java/com/gridapp/territory_mapper/model/company.java
@@ -3,8 +3,28 @@ package com.gridapp.territory_mapper.model;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
-@
+
+//annotations
+@Entity //Declares this class as a database entity
+@Table(name = "companies") //Maps it to a table called "companies
+@Data //Auto-generates getters, setters, toString(),
+@NoArgsConstructor // Adds a no-argument constructor
+@AllArgsConstructor //Adds a constructor with all fields
+@Builder //Allows you to use the builder pattern
+
 public class company {
-  
+  // this is the primary key and it is generating a number to store it
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String companyName;
+    private String name;
+    private String address;
+    private String city;
+    private String state;
+
+
+
 }
